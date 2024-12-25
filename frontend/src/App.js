e68b9cad9a77953c43b178f15e36caac09c6daac
Chirag Patil <160267821+chirag-patil625@users.noreply.github.com>
2024-12-25 11:38:29 +0300

    Delete frontend directory

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="text-center p-6 bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold">Hello, Tailwind!</h1>
-    </div>
-  );
-}
-
-export default App;
